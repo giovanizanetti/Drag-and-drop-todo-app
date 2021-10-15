5ce419581a59d4cbadfc291a52638493ad352411
giovanizanetti <zanetti.giovani@gmail.com>
2021-10-15 17:52:11 +0200

    add header to the page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import TodoList from './components/TodoList/TodoList'
-import { Container, Col } from 'react-bootstrap'
+import { Container, Col, Navbar } from 'react-bootstrap'
 
 function App() {
   return (
-    // <div className='container'>
-    //   <TodoList />
-    // </div>
-    <Container className='d-flex justify-content-center mt-5'>
-      <Col s={12} md={8} xl={6}>
-        <TodoList />
-      </Col>
-    </Container>
+    <>
+      <Navbar title='header' bg='dark'>
+        THis is the header
+      </Navbar>
+      <Container className='d-flex justify-content-center mt-5'>
+        <Col s={12} md={8} xl={6}>
+          <TodoList />
+        </Col>
+      </Container>
+    </>
   )
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -4,3 +4,9 @@ import App from './App'
 test('renders learn react link', () => {
   render(<App />)
 })
+test('renders header without crashing', () => {
+  render(<App />)
+  const header = screen.getByTitle('header')
+
+  expect(header).toBeInTheDocument()
+})
