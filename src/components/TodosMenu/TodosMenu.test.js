0b97eb10150feefac74fd9993c7140538664d03a
giovanizanetti <zanetti.giovani@gmail.com>
2021-10-22 16:36:03 +0200

    fix test

--- src/components/TodosMenu/TodosMenu.test.js
+++ src/components/TodosMenu/TodosMenu.test.js
@@ -10,7 +10,7 @@ import TodoMenu from './TodosMenu.js'
 describe('TodoMenu', () => {
   test('should todo Menu render without crashing', () => {
     render(<TodoMenu />)
-    // const todo = screen.getByTitle('todo')
+    // const todo = screen.getByTitle('todo-menu')
     // expect(todo).toHaveTextContent('my todo')
     // expect(todo).toBeInTheDocument()
   })
